3378e13caa075b0e65897e423bb24458e868f49f
Piotr Bobinski <piotr.bobinski@ericsson.com>
2018-01-25 12:40:23 +0100

    first commit, init repo

---
+++ src/main/java/com/bobzone/elasticspring/ElasticSpringApplication.java
@@ -0,0 +1,12 @@
+package com.bobzone.elasticspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSpringApplication.class, args);
+	}
+}
